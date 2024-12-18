ebc94f32d1b6f4ebedfbdd2e68e275050f406abe
Mo <damoye1993@hotmail.com>
2024-12-19 03:20:37 +0800

    support wsl and refact mason

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,16 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
   lazy = vim.fn.argc(-1) == 0,
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  init = function(plugin)
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
+  end,
   opts = {
-    ensure_installed = { "go", "lua", "markdown_inline" },
+    ensure_installed = { "bash", "go", "lua", "markdown_inline", "vim", "vimdoc" },
     auto_install = true,
     highlight = { enable = true },
+    incremental_selection = { enable = true },
+    indent = { enable = true },
     textobjects = {
       select = {
         enable = true,
@@ -18,44 +24,18 @@ return {
           ["ic"] = "@class.inner",
         },
       },
-      swap = {
-        enable = true,
-        swap_next = { ["<Leader>a"] = "@parameter.inner" },
-        swap_previous = { ["<Leader>A"] = "@parameter.inner" },
-      },
       move = {
         enable = true,
         set_jumps = true,
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
+        goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
+        goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
+        goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
+        goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" },
       },
     },
-    indent = { enable = true },
   },
   config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function()
-        if pcall(vim.treesitter.start) then
-          vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-          vim.opt_local.foldmethod = "expr"
-        end
-      end,
-    })
   end,
   build = ":TSUpdate",
 }
