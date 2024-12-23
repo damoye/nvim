9ced3fdf61c164540b0a869eeed8bc602076eca6
damo.ydm <damo.ydm@alibaba-inc.com>
2024-12-24 01:04:19 +0800

    init
    Change-Id: I52908e67aac9d2e3ec16cb25de8d1ca18d748105

---
+++ lua/plugins/core.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        go = { "gofumpt" }, -- not use goimports for too slow at orginizing imports
+      },
+    },
+  },
+  {
+    "stevearc/overseer.nvim",
+    opts = {
+      templates = { "user.go_build", "user.run", "builtin" },
+    },
+  },
+}
